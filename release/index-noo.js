69e658aa30e0fbd59283cfd964126db8005e583a
Travis CI <travis@travis-ci.org>
2020-12-26 13:26:41 +0000

    Travis update:  (Build 16)
    [skip ci]

--- release/index-noo.js
+++ release/index-noo.js
@@ -2764,28 +2764,40 @@ function transmute(x) {
     return x;
 }
 let unwrap = Symbol();
-let fw = new Proxy(class {
-}, {
-    construct: (o, args, t)=>(eval('navigator').serviceWorker.register(args[0]).then((x)=>eval('navigator').serviceWorker.x = x
-        ), eval('navigator').serviceWorker)
-});
-let fw2;
-try {
-    fw2 = argv[0] == 'worker' ? undefined : w({
-        Worker: transmute(fw),
-        prepend: '',
-        argv: [
-            'worker'
-        ]
+(async ()=>{
+    let fw = new Proxy(class {
+    }, {
+        construct: (o, args, t)=>(eval('navigator').serviceWorker.register(args[0]).then((x)=>eval('navigator').serviceWorker.x = x
+            ), eval('navigator').serviceWorker)
     });
-} catch (err) {
-}
-let wb_ = argv[0] == 'worker' ? import('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js').then((x)=>true
-) : Promise.resolve(false);
-if (argv[0] == 'worker') Object.keys(self.workbox).map((x)=>self.workbox[x]
-);
-let wb = await wb_;
-if (wb) self.workbox.recipies.offlineFallback();
+    let fw2;
+    try {
+        fw2 = argv[0] == 'worker' ? undefined : w({
+            Worker: transmute(fw),
+            prepend: '',
+            argv: [
+                'worker'
+            ]
+        });
+    } catch (err) {
+    }
+    let wb_ = argv[0] == 'worker' ? import('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js').then((x)=>true
+    ) : Promise.resolve(false);
+    if (argv[0] == 'worker') Object.keys(self.workbox).map((x)=>self.workbox[x]
+    );
+    let wb = await wb_;
+    if (wb) self.workbox.recipies.offlineFallback();
+    if (fw2 !== undefined) {
+    }
+    if (argv[0] !== "nohash") {
+        try {
+            let hash = eval('location').hash;
+            let h64 = atob(hash);
+            let parsed = JSON.parse(h64);
+        } catch (err) {
+        }
+    }
+})();
 !(function() {
     "use strict";
     try {
